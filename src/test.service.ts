dd6d041cd067938617a9e17374dc1693b969ec40
jimao <shihao.gu@perfma.com>
2024-11-16 16:01:10 +0800

    复制过来

---
+++ src/test.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TestService {
+  private db: string[] = [];
+
+  public list() {
+    return this.db.slice();
+  }
+
+  public add(text: string) {
+    this.db.push(text);
+  }
+}
